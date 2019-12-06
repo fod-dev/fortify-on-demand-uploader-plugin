988564341f1484df377226430703e0b698af3d45
Vodrie Cohen <vodrie.cohen@microfocus.com>
2019-12-05 20:20:49 -0500

    [FedRampConfidential] Fixing polling for results menu. Fixing timeout issue. TODO Resolve the "compilation error"

--- src/main/java/org/jenkinsci/plugins/fodupload/FodApiConnection.java
+++ src/main/java/org/jenkinsci/plugins/fodupload/FodApiConnection.java
@@ -17,7 +17,7 @@ public class FodApiConnection {
 
     private final static int CONNECTION_TIMEOUT = 30; // seconds
     private final static int WRITE_TIMEOUT = 30; // seconds
-    private final static int READ_TIMEOUT = 30; // seconds
+    private final static int READ_TIMEOUT = 90; // seconds
     public final static int MAX_SIZE = 50;
     
 
