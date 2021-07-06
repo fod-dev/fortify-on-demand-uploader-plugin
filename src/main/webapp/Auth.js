c931ed7b592a3013e6b9b517bdc705d65138c974
arixmf <arik.lekar@microfocus.com>
2021-07-06 10:32:12 -0400

    fix auth override and fix releaseId/bsiToken persistence

---
+++ src/main/webapp/Auth.js
@@ -0,0 +1,31 @@
+jq = jQuery;
+
+const AuthInfoChangedEvent = new CustomEvent('AuthInfoChanged');
+
+function getAuthInfo() {
+    return {
+        overrideGlobalAuth: jq('[name="overrideGlobalConfig"]').is(':checked'),
+        username: jq('#usernameField').val(),
+        accessTokenKey: jq('[name="_.personalAccessToken"]').val(),
+        tenantId: jq('#tenantIdField').val()
+    };
+}
+
+function onAuthInfoChanged(cb) {
+    document.removeEventListener('AuthInfoChanged', cb);
+    document.addEventListener('AuthInfoChanged', cb);
+}
+
+function sendChangedEvent() {
+    console.log('changed');
+    debounce(() => document.dispatchEvent(AuthInfoChangedEvent), 500)();
+}
+
+function init() {
+    jq('[name="overrideGlobalConfig"]').off('change').change(() => sendChangedEvent());
+    jq('#usernameField').off('change').change(() => sendChangedEvent());
+    jq('[name="_.personalAccessToken"]').off('change').change(() => sendChangedEvent());
+    jq('#tenantIdField').off('change').change(() => sendChangedEvent());
+}
+
+spinAndWait(() => jq('[name="overrideGlobalConfig"]').val()).then(init);
