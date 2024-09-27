8f17af97903aaa7da37a890acec06d0f8a4f0a99
Ankit Rathod <arathod3@opentext.com>
2024-09-27 11:55:32 +0530

    Added appropriate log lines to performPackaging.

--- src/main/java/org/jenkinsci/plugins/fodupload/PayloadPackaging.java
+++ src/main/java/org/jenkinsci/plugins/fodupload/PayloadPackaging.java
@@ -34,16 +34,13 @@ public interface PayloadPackaging {
 
 final class PayloadPackagingImpl {
     static FilePath performPackaging(SastJobModel model, String technologyStack, Boolean openSourceAnalysis, String globalSCPath, FilePath workspace, PrintStream logger) throws IOException {
-        logger.println("Starting Zip Packaging for source @ " + model.getSrcLocation());
         FilePath srcLocation = new FilePath(workspace, model.getSrcLocation());
         File payload;
-
         if (model.getSelectedScanCentralBuildType().equalsIgnoreCase(FodEnums.SelectedScanCentralBuildType.None.toString())) {
-
+            logger.println("Starting Zip Packaging for source @ " + model.getSrcLocation());
             if (ValidationUtils.isScanCentralRecommended(technologyStack)) {
                 logger.println("\nFortify recommends using ScanCentral Client to package code for comprehensive scan results.\n");
             }
-
             // zips the file in a temporary location
             payload = Utils.createZipFile(technologyStack, srcLocation, logger);
             if (payload.length() == 0) {
